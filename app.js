1cabcecbd1674c822bc61306bb5ad6d44760da4a
Nizaam Dalwai <54627453+pwem-chi@users.noreply.github.com>
2020-01-09 19:46:15 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 //set up express app
 const app = express();
 
-app.use(bodyParser.json());
-
-//initialise routes
-app.use('/api', require('./routing/api'))
-
+//listen for requests
 app.listen(process.env.port || 6969, function(){
-    console.log("now listening for requests");
+    console.log('Confirmed, now listening to port 6969')
 });
